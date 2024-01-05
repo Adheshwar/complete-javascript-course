bf8e6b9e55d96e488bb72c560661be7115850683
Adheshwar <adheshwar@hotmail.com>
2024-01-05 20:43:44 +0530

    Parameter defaults

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function(flightNum, numPassengers=1, price=2000){
+
+    /*ES5
+    numPassengers = numPassengers || 1;
+    price = price || 2000;
+    */
+
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    };
+
+    console.log(booking);
+    bookings.push(booking);
+};
+
+createBooking('AI2456');
